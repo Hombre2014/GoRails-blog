e31377544c0f8b1036dafbfa0f4a40a1c0f34d45
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2024-10-07 11:48:07 +0200

    Add tests for public help methods

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -1,7 +1,47 @@
 require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "draft? returns true for draft blog posts" do
+    # assert draft_blog_post.draft? # Using the helper method
+    # OR using the fixture
+    assert blog_posts(:draft).draft?
+  end
+
+  test "draft? returns false for published blog posts" do
+    assert_not blog_posts(:published).draft?
+    # OR
+    # refute BlogPost.new(published_at: 1.month.ago).draft?
+  end
+
+  test "draft? returns false for scheduled blog posts" do
+    refute blog_posts(:scheduled).draft?
+  end
+
+  test "published? returns true for published blog posts" do
+    assert blog_posts(:published).published?
+  end
+
+  test "published? returns false for draft blog posts" do
+    refute draft_blog_post.published?
+  end
+
+  test "published? returns false for scheduled blog posts" do
+    refute blog_posts(:scheduled).published?
+  end
+
+  test "scheduled? returns true for scheduled blog posts" do
+    assert blog_posts(:scheduled).scheduled?
+  end
+
+  test "scheduled? returns false for draft blog posts" do
+    refute draft_blog_post.scheduled?
+  end
+
+  test "scheduled? returns false for published blog posts" do
+    refute blog_posts(:published).scheduled?
+  end
+
+  def draft_blog_post
+    BlogPost.new(published_at: nil)
+  end
 end
