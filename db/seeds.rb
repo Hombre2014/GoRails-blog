0da7ef7f768058f0e8bc162195349a346ad6603f
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2024-10-07 17:17:36 +0200

    Add pagy for pagynation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# 62.times do |i|
+#   blog_post = BlogPost.where(title: "Blog Post #{i + 1}").first_or_initialize
+#   blog_post.update(content: "This is the content for Blog Post #{i + 1}, published_at: Time.current - #{i} days") if blog_post.new_record?
+#   blog_post.save!
+# end
